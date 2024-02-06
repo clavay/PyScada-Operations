72b16c8c3f4ef18071472d6b8c019207d46bfb2f
clavay <clavayssiere@univ-pau.fr>
2024-02-06 14:51:17 +0100

    update pysada v0.8.2 requirement

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     platforms=["OS Independent"],
     classifiers=CLASSIFIERS,
     install_requires=[
-        "pyscada>=0.8.1",
+        "pyscada>=0.8.2",
         "numpy",
         "simpleeval",
     ],
